0710ef55608711996c342f27b922a3eec3738614
Vyctor <vyctorguimaraes@gmail.com>
2020-04-28 22:29:19 -0300

    Navigating to create products

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+
+// Routes
 import { ProductCrudComponent } from "./views/product-crud/product-crud.component";
 import { HomeComponent } from "./views/home/home.component";
+import { ProductCreateComponent } from "./components/product/product-create/product-create.component";
 
 const routes: Routes = [
   {
@@ -11,6 +14,10 @@ const routes: Routes = [
   {
     path: "products",
     component: ProductCrudComponent,
+  },
+  {
+    path: "products/create",
+    component: ProductCreateComponent,
   },
 ];
 
